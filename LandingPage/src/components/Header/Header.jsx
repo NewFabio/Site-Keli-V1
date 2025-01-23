1cae8f02fa13bf34cb296a4d4d24b2f0da9f4da3
Fabio Conceição <fabio.conceicaoads@gmail.com>
2025-01-23 14:08:29 -0300

    att header

--- LandingPage/src/components/Header/Header.jsx
+++ LandingPage/src/components/Header/Header.jsx
@@ -1,24 +1,30 @@
 
 function Header() {
     return (
-        <header className="navbar">
+        <div className="navbar navbar-expand-md navbar-white bg-transparent fixed-top">
             <nav className="container">
-                <a className="navbar-brand">Logo</a>
-                
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#humburguer" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        
-                        <span className="navbar-toggle-icon"></span>
-                    </button>
-                <div className="collapse navbar-collapse" id="humburguer">
+                <span className="navbar-brand text-white" style={{fontWeight: '200'}} >Estúdio<span style={{fontFamily:'Playwrite in', fontWeight:'400'}}>Delicata</span></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHumburguerMenu" aria-controls="navbarHumburguerMenu" aria-expanded="false" aria-label="Toggle navigation" style={{border: 'none'}}>
+                    <span className="navbar-toggler-icon bg-white" style={{}}></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarHumburguerMenu">
                     <ul className="navbar-nav">
-                        <li className="nav-item"><a className="nav-link active">Home</a></li>
-                        <li className="nav-item"><a className="nav-link">About</a></li>
-                        <li className="nav-item"><a className="nav-link">Services</a></li>
-                        <li className="nav-item"><a className="nav-link">Contact</a></li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link" aria-current="page">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link" aria-current="page">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link" aria-current="page">Services</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link" aria-current="page">Contact</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
-        </header>
+        </div>
     )
 }
 
