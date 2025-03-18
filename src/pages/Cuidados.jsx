123ee8ee80686505882ccb56972d5a5d66919e1f
NewFabio <fabio.conceicaoads@gmail.com>
2025-03-17 21:29:46 -0300

    .

--- src/pages/Cuidados.jsx
+++ src/pages/Cuidados.jsx
@@ -9,7 +9,7 @@ const Cuidados = () => {
                         <div className="card-body text-center">
                             <h5 className="card-title">Cuidados Pós procedimento de Remoção de tatuagens e micropigmentação</h5>
                             <p className="card-text"></p>
-                            <a type="button" className="btn btn-primary" href="https://drive.google.com/file/d/1_kP-BrcD5ZVIPajoqc4cwLAZzAB7NpgC/view?usp=drivesdk" target="_blank">Ler mais</a>
+                            <a type="button" className="btn btn-primary" alt='link para dowload remoção de micro e tatuagens' href="https://drive.google.com/file/d/1_kP-BrcD5ZVIPajoqc4cwLAZzAB7NpgC/view?usp=drivesdk" target="_blank">Ler mais</a>
                         </div>
                     </div>
                 </div>
@@ -18,13 +18,13 @@ const Cuidados = () => {
                         <div className="card-body text-center">
                             <h5 className="card-title">Cuidados Pós procedimento de Micropigmentação</h5>
                             <p className="card-text"></p>
-                            <a type="button" className="btn btn-primary" href="https://drive.google.com/file/d/1_nqTFG7sJyw3arw1QkTdi59H_SGTCazX/view?usp=drivesdk" target="_blank">Ler mais</a>
+                            <a type="button" className="btn btn-primary" alt='link para dowload pós procedimento cuidados com a micropigmentação' href="https://drive.google.com/file/d/1_nqTFG7sJyw3arw1QkTdi59H_SGTCazX/view?usp=drivesdk" target="_blank">Ler mais</a>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="ratio ratio-16x9 container-sm">
-                <iframe src="https://www.youtube.com/embed/ROwSBhG2INs?si=ZLESqv1FK23RtM5Z" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                <iframe src="https://www.youtube.com/embed/ROwSBhG2INs?si=ZLESqv1FK23RtM5Z" alt='video youtube' title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
             </div>
         </article>
     );
