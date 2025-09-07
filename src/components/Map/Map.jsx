1501ce5e21abea3abcf53360964487603223ced2
NewFabio <fabio.conceicaoads@gmail.com>
2025-09-07 14:09:55 -0300

    create .env for API KEY

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,7 +7,7 @@ const Map = () => {
 
     const  { isLoaded }  =  useJsApiLoader ( { 
         id : 'google-map-script' , 
-        googleMapsApiKey : 'AIzaSyApC1k8fYSP8To999NnhOmzq_VjvhoiaO0' , 
+        googleMapsApiKey : import.meta.env.VITE_GOOGLE_MAPS_API_KEY ,
       } )
 
       const  Delicata  =  { 
